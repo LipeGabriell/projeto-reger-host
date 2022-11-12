c97e8392ec241f3f5d5b3c8cee27903865d59ea8
LipeGabriell <felipe.ramiressanches@hotmail.com>
2022-11-12 16:55:53 -0300

    AOE bugs

--- frontend/src/assets/Components/Containers/Sobre/sobre.tsx
+++ frontend/src/assets/Components/Containers/Sobre/sobre.tsx
@@ -3,8 +3,10 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Lixo1 from '../../../images/lixo1.png'
 import Lixo1Mobile from '../../../images/lixo1Mobile.png'
+
 import Lixo2 from '../../../images/lixo2.png'
 import Lixo2Mobile from '../../../images/lixo2Mobile.png'
+
 import Lixo3 from '../../../images/lixo3.png'
 import Lixo3Mobile from '../../../images/lixo3Mobile.png'
 
@@ -15,12 +17,7 @@ function Sobre() {
             <section id='sobre' className='flex font column'>
                 <div id='txtSobre'>
                     <h1><span>SOBRE</span> NÓS</h1>
-                    <p>O Centro Universitário Sagrado Coração tem uma preocupação constante com os resíduos produzidos
-                        em todas as suas atividades. Essa preocupação vem de uma necessidade não só da instituição, mas
-                        um cuidado com os impactos causados no meio ambiente e com os efeitos a curto e longo prazo. O
-                        projeto Reger é um programa realizado pela Unisagrado afim de tomar medidas para reduzir o
-                        consumo de resíduos, fazendo com que diminua a quantidade os rejeitos produzidos no campus,
-                        incentivando o descarte correto.</p>
+                    <p>O Centro Universitário Sagrado Coração tem uma preocupação constante com os resíduos produzidos em todas as suas atividades. Essa preocupação vem de uma necessidade não só da instituição, mas um cuidado com os impactos causados no meio ambiente e com os efeitos a curto e longo prazo. O projeto Reger é um programa realizado pela Unisagrado afim de tomar medidas para reduzir o consumo de resíduos, fazendo com que diminua a quantidade os rejeitos produzidos no campus, incentivando o descarte correto.</p>
                 </div>
 
                 <div id='imgSobre'>
