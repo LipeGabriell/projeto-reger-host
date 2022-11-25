873e89db24216deda5e9866f02a50347100c69b1
LipeGabriell <felipe.ramiressanches@hotmail.com>
2022-11-25 13:31:00 -0300

    alterado font // dicas

--- frontend/src/assets/Components/Containers/Dicas/dicas.tsx
+++ frontend/src/assets/Components/Containers/Dicas/dicas.tsx
@@ -21,24 +21,28 @@ function Dicas() {
 
                 <div id='dicasEsquerda' className='flex'>
                     <img src={Reciclar} alt="" />
+                    <h3>RECICLAR</h3>
                     <p>Envolve a transformação dos materiais para a produção de matéria-prima para outros produtos por meio de processos industriais ou artesanais. É fabricar um produto a partir de um material usado.
                     </p>
                 </div>
 
                 <div id='dicasMeio' className='flex'>
-                    <h2>Dicas</h2>
+                    <h2>DICAS</h2>
                     <div id='dicasCima'>
                         <img src={Reutilizar} alt="" />
+                        <h3>REUTILIZAR</h3>
                         <p>Usar novamente as embalagens.</p>
                     </div>
                     <div id='dicasBaixo'>
                         <img src={Reduzir} alt="" />
+                        <h3>REDUZIR</h3>
                         <p>Consumir menos produtos e preferir aqueles que ofereçam menor potencial de geração de resíduos e tenham maior durabilidade.</p>
                     </div>
                 </div>
 
                 <div id='dicasDireita' className='flex'>
                     <img src={Repensar} alt="" />
+                    <h3>REPENSAR</h3>
                     <p>Sobre os seus atos de consumo e os impactos que eles provocam sobre você mesmo, a economia, as relações sociais e a natureza.</p>
 
                 </div>
